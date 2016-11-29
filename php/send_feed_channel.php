c0e36d2d04ba5fbdfb465595fa515a72bb8acb1b
Chris Wiseman <cjwiseman11@gmail.com>
2016-11-29 20:15:29 +0000

    Added preliminary safety to input

--- php/send_feed_channel.php
+++ php/send_feed_channel.php
@@ -1,7 +1,7 @@
 <?php
 	// Load configuration file outside of doc root
 	$root = $_SERVER['DOCUMENT_ROOT'];
-	$config = parse_ini_file($root . '/../config.ini'); 
+	$config = parse_ini_file($root . '/../config.ini');
 
 	//Connecting to sql db.
 	$connection = mysqli_connect("localhost",$config['username'],$config['password'],$config['dbname']);
@@ -10,8 +10,8 @@
 		//TODO: Add error
 	}
 
-	$feed = $_POST[feed_link];
-	$channel = $_POST[feed_channel];
+	$feed = mysqli_real_escape_string($connection,$_POST[feed_link]);
+	$channel = mysqli_real_escape_string($connection,$_POST[feed_channel]);
 	libxml_use_internal_errors(true);
 	$feed_to_array = simplexml_load_file($feed);
 	if(false === $feed_to_array){
@@ -23,29 +23,29 @@
 		//Sending form data to sql db.
 		$newsFeedExist = mysqli_query($connection,"SELECT * FROM newsfeeds WHERE rsslink = '$feed'");
 		$newsFeedExist_rows = $newsFeedExist->num_rows;
-		
+
 		$chanid = mysqli_query($connection,"SELECT * FROM channels WHERE channame = '$channel'");
 		$num_rows = $chanid->num_rows;
 		if ($num_rows === 0){
 			mysqli_query($connection,"INSERT INTO channels (channame) VALUES ('$channel')");
 		}
-		
+
 		if ($newsFeedExist_rows === 0){
 			mysqli_query($connection,"INSERT INTO newsfeeds (rsslink, rssSrcSite)
 			VALUES ('$_POST[feed_link]','$autoSiteLink')");
 		}
-		
+
 		$chanExist = mysqli_query($connection,"
-		SELECT * FROM `channelfeed-links` 
-		WHERE newsfeedid = (SELECT id FROM newsfeeds WHERE rsslink='$feed') 
+		SELECT * FROM `channelfeed-links`
+		WHERE newsfeedid = (SELECT id FROM newsfeeds WHERE rsslink='$feed')
 		AND channelid = (SELECT id FROM channels WHERE channame='$channel')");
 		$channum_rows = $chanExist->num_rows;
-		
+
 		if ($channum_rows === 0){
 			mysqli_query($connection,"INSERT INTO `channelfeed-links`(newsfeedid,channelid)
 			VALUES ((SELECT id FROM newsfeeds WHERE rsslink='$feed'),
 			(SELECT id FROM channels WHERE channame='$channel'));");
-		}	
+		}
 	}
 
 	//close the db connection
