988c2fc7868bc7ca4ae69370f975b708ba1864ea
Chris Wiseman <cjwiseman11@gmail.com>
2016-12-12 22:32:47 +0000

    Fixed an issue with deleteScript not deleting

--- _rootfiles/deleteScript.php
+++ _rootfiles/deleteScript.php
@@ -1,6 +1,7 @@
 <?php
 ini_set('user_agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:16.0) Gecko/20100101 Firefox/16.0');
-$config = parse_ini_file('config.ini'); 
+
+$config = parse_ini_file('config.ini');
 
 //Connecting to sql db.
 $connection = mysqli_connect("localhost",$config['username'],$config['password'],$config['dbname']);
@@ -11,26 +12,28 @@ if($connection === false){
 $sql = "UPDATE posts SET archived = true WHERE date <= (NOW() - INTERVAL 1 MONTH) AND archived = 0";
 mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
 
-$getrows = "SELECT imgSrc FROM posts WHERE date <= (NOW() - INTERVAL 1 MONTH) AND archived = 0";
+$getrows = "SELECT imgSrc FROM posts WHERE date <= (NOW() - INTERVAL 1 MONTH) AND archived = 1";
 $result = mysqli_query($connection, $getrows) or die("Error in Selecting " . mysqli_error($connection));
 $deleted = 0;
 
 while($row = mysqli_fetch_assoc($result)){
     $deleted++;
-    if(unlink($root . "/feedify/thumbnails/full/" . $row['imgSrc']))
+    if(unlink($root . "/feedify/thumbnails/full/" . $row['imgSrc'])){
         echo "File Deleted.";
+      }
 }
 
 echo "Successfully archived $deleted assets";
 
-$getrows = "SELECT imgSrc FROM posts WHERE date <= (NOW() - INTERVAL 2 MONTH) AND date > (NOW() - INTERVAL 1 MONTH) AND archived = 0";
+$getrows = "SELECT imgSrc FROM posts WHERE date <= (NOW() - INTERVAL 2 MONTH) AND archived = 1";
 $result = mysqli_query($connection, $getrows) or die("Error in Selecting " . mysqli_error($connection));
 $deleted = 0;
 
 while($row = mysqli_fetch_assoc($result)){
     $deleted++;
-    if(unlink($root . "/feedify/thumbnails/" . $row['imgSrc']))
+    if(unlink($root . "/feedify/thumbnails/" . $row['imgSrc'])){
         echo "File Deleted.";
+      }
 }
 
 echo "Successfully archived $deleted assets";
