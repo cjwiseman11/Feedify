f160cdcaff9686d932e52e8dcbbe8eff9aa2cc9b
Chris Wiseman <cjwiseman11@gmail.com>
2017-04-19 22:44:27 +0100

    Updated feeds submit design

--- feedify1.1/js/rssreaderscript.js
+++ feedify1.1/js/rssreaderscript.js
@@ -1,36 +1,8 @@
-function sendRSS() {
-	if($('#rssfeedsubmit').val() == ""){
-		$('.alert-danger').removeClass('hide');
-		return false;
-	}else{
-		$('.alert-danger').addClass('hide');
-		return true;
-	}
-}
-
-//FB Share Button Click JS
-$('.btnShare').click(function(){
-	elem = $(this);
-	postToFeed(elem.data('title'), elem.data('desc'), elem.prop('href'), elem.data('image'));
-	return false;
-});
-
-//Click Tracking for articles
-$('.articlelink').click(function(){
-	var link = $(this).attr("href");
-	$.get("php/linkClickTrack.php?link=" + link);
-});
-
-//Toggle feed list in/out
-$('#seefeedlist').click(function(){
-   $('#feedlist').toggle(500);
-});
-
 //Save for later button
 $('.save-for-later').click(function(e){
 	e.preventDefault();
 	var id = $(this).parent().closest('div[id]').attr("id");
-	$.get("php/saveforlater.php?id=" + id);
+	$.get("statements/saveforlater.php?id=" + id);
 	$(this).after("Saved");
 	$(this).remove();
 });
@@ -38,38 +10,9 @@ $('.save-for-later').click(function(e){
 $('.remove-from-saved').click(function(e){
 	e.preventDefault();
 	var id = $(this).parent().closest('div[id]').attr("id");
-	$.get("php/removefromsaved.php?id=" + id);
+	$.get("statements/removefromsaved.php?id=" + id);
 	$('#'+id).text("Removed");
 });
-
-//Random Button Logic
-$('#randomify').click(function(){
-    var randomLink = "";
-    var oReq = new XMLHttpRequest(); //New request object
-    oReq.onload = function() {
-        randomLink = this.responseText; //Will alert: 42
-        window.location.href = JSON.parse(randomLink);
-    };
-    oReq.open("get", "php/randomArticle.php", true);
-    $(this).text('Loading...');
-    oReq.send();
-    return false;
-});
-
-//Login click
-$('.login-btn').on("click", function(){
-	//$('#member-login > form').toggleClass('hidden-sm hidden-xs');
-	//	$(this).text($('#member-login > form').hasClass("hidden-sm") ? "Login" : "Close");
-	if($('#member-login > form').css('display') == 'none'){
-		$('.login-btn').text("Close");
-		$('#member-login > form').show();
-	} else if($('#member-login > form').css('display') == 'block'){
-		$('.login-btn').text("Log in");
-		$('#member-login > form').hide();
-	}
-	//$('#member-login > form').slideToggle(300);
-});
-//Check FirstTime Var
 
 $(document).ready(function(){
 
@@ -81,16 +24,16 @@ $(document).ready(function(){
 
 		$('#feedLink').on("change keyup paste", function(){
 			if(!($('#feedLink').val().length < 2)){
-				$.get("php/search.php?type=rows&val=" + $('#feedLink').val(), function(response){
+				$.get("statements/search.php?type=rows&val=" + $('#feedLink').val(), function(response){
 						if(response.length == 0){
-							$.get("php/search.php?type=url&val=" + $('#feedLink').val(), function(urlresponse){
+							$.get("statements/search.php?type=url&val=" + $('#feedLink').val(), function(urlresponse){
 								if(urlresponse == "false"){
 									$('.search-results').html("<p>We do not currently have a match for this feed.</p><p>If you enter a full URL, we can try and find it for you and add it!</p>");
 								} else {
 									$('.search-results').html("<a href class='rss-search'>Click here to search " + $('#feedLink').val() + " for feed</a>");
 									$('.rss-search').on("click",function(e){
 										e.preventDefault();
-										$.get("php/search.php?type=rss&val=" + $('#feedLink').val(), function(rssresponse){
+										$.get("statements/search.php?type=rss&val=" + $('#feedLink').val(), function(rssresponse){
 											if(!(rssresponse.length == 0)){
 												$('.search-results').html(rssresponse);
 											} else {
@@ -111,9 +54,9 @@ $(document).ready(function(){
 
 		//RSS Live checker
 		$('#rssfeedsubmit').on("change keyup paste", function(){
-			$.get("php/search.php?type=url&val=" + $('#rssfeedsubmit').val(), function(urlresponse){
+			$.get("statements/search.php?type=url&val=" + $('#rssfeedsubmit').val(), function(urlresponse){
 				if(!(urlresponse == "false")){
-					$.get("php/rss-validator.php?val=" + $('#rssfeedsubmit').val(), function(rssresponse){
+					$.get("statements/rss-validator.php?val=" + $('#rssfeedsubmit').val(), function(rssresponse){
 						if(rssresponse == "true"){
 							$('#rssresponse-helpblock').show();
 							$('#rssfeedsubmit').parent().addClass("has-success");
@@ -151,7 +94,7 @@ $(document).ready(function(){
 
 		//Channel live checker
 		$('#new-channel-entry').on("change keyup paste", function(){
-			$.get("php/search.php?type=channel&val=" + $('#new-channel-entry').val(), function(channelresponse){
+			$.get("statements/search.php?type=channel&val=" + $('#new-channel-entry').val(), function(channelresponse){
 				if(channelresponse == "fail"){
 					$('#new-channel-message').show();
 					$('#new-channel-message').parent().addClass("has-error");
--- feedify1.1/templates/feeds.php
+++ feedify1.1/templates/feeds.php
@@ -1,79 +1,82 @@
 	<div class="container">
-        <?php
+	<?php
     if(!isset($_SESSION['feedifyusername'])): ?>
 		<p>Oi Login!</p>
 		<p>Submitting feeds and creating channels is only allowed by members</p>
   <?php else : ?>
-      <script type="text/javascript" src="js/rssreaderscript.js"></script>
-		<div id="feed-section" class="col-sm-12">
-	    <h2><small>Submit Feeds and Create Channels</small></h2>
-	    <p>Hello <?php echo $_SESSION['feedifyusername']; ?>, here you can create your very own channels for Feedify and submit news feeds to each one</p>
-	    <p>You can choose to make your channel private, which will mean it won't appear in "All" or as a public channel</p>
-	    <p>Below you can also find a list of feeds already found on the site, which you can add to your channels or a public channel if appropriate</p>
-	    <hr>
-	  </div>
-		<div class="submit-search-rss-section col-sm-12">
-	    <div class="col-sm-6 submit-section <?php if($_GET['error'] == 'exists'){ echo 'has-error'; }?>">
+		<script type="text/javascript" src="js/rssreaderscript.js"></script>
+		<div class="submit-search-rss-section columns">
+	    <div class="column submit-section <?php if($_GET['error'] == 'exists'){ echo 'has-error'; }?>">
 	      <form action="statements/send_feed_channel.php" method="post" onsubmit="return sendRSS();" class="form col-sm-8">
-	      <div class="form-group">
-	        <label class="control-label" for="feed_link">Please input a full RSS Link including HTTP, or <a href="#search-area" id="search-area-link">use the search area</a></label>
-	        <input type="text" value="<?php echo $_GET['feed']; ?>" name="feed_link" class="form-control" id="rssfeedsubmit" placeholder="RSS URL" required>
+	      <div class="field">
+	        <label class="label" for="feed_link">Please input a full RSS Link including HTTP, or <a href="#search-area" id="search-area-link">use the search area</a></label>
+	        <input type="text" value="<?php echo $_GET['feed']; ?>" name="feed_link" class="input" id="rssfeedsubmit" placeholder="RSS URL" required>
 	        <span class='help-block' id='rssresponse-helpblock'>Valid Feed Detected! Yay!</span>
 	      </div>
-				<select name="channel-selector" id="channel-selector" class="form-control">
-				  <option>Choose a channel</option>
-				  <option id="create-channel">Create your own...</option>
-				 	<optgroup label="Your Channels">
-						<?php foreach(getMembersChannels($_SESSION['feedifyusername']) as $row){
-							if($row['channame'] == $_GET['chan']){
-								echo "<option selected>" . $row['channame'] . "</option>";
-							} else {
-								echo "<option>" . $row['channame'] . "</option>";
-							}
-						}?>
-					</optgroup>
-				 	<optgroup label="All Channels">
-						<?php foreach(getChannelsList() as $row){
-							if($row['channame'] == $_GET['chan']){
-								echo "<option selected>" . $row['channame'] . "</option>";
-							} else {
-								echo "<option>" . $row['channame'] . "</option>";
-							}
-						}?>
-					</select>
-				</optgroup>
-				<input type="text" name="new-channel" class="form-control" id="new-channel-entry" placeholder="New channel here">
-				<span class='help-block' id='new-channel-message'>This channel already exists.</span>
-				<div id="private-channel-radio" class="radio-selection">
-					<br>
-					<p>Would you like this channel to be hidden or public?</p>
-		      <div class="radio">
-		        <label>
-		          <input type="radio" name="optionsRadios" id="optionsRadios1" value="1">
-		          Keep this channel hidden please
-		        </label>
-		      </div>
-		      <div class="radio">
-		        <label>
-		          <input type="radio" name="optionsRadios" id="optionsRadios2" value="0" checked>
-		          Make the channel public, for all to see
-		        </label>
-		      </div>
+				<div class="field">
+					<span class="select">
+						<select id="channel-selector" name="channel-selector" >
+							<option>Choose a channel</option>
+							<option id="create-channel">Create your own...</option>
+							<optgroup label="Your Channels">
+								<?php foreach(getMembersChannels($_SESSION['feedifyusername']) as $row){
+									if($row['channame'] == $_GET['chan']){
+										echo "<option selected>" . $row['channame'] . "</option>";
+									} else {
+										echo "<option>" . $row['channame'] . "</option>";
+									}
+								}?>
+							</optgroup>
+							<optgroup label="All Channels">
+								<?php foreach(getChannelsList() as $row){
+									if($row['channame'] == $_GET['chan']){
+										echo "<option selected>" . $row['channame'] . "</option>";
+									} else {
+										echo "<option>" . $row['channame'] . "</option>";
+									}
+								}?>
+							</optgroup>
+						</select>
+					</span>
 				</div>
-				<br>
+				<div class="field">
+					<input type="text" name="new-channel" class="input" id="new-channel-entry" placeholder="New channel here">
+					<span class='help-block' id='new-channel-message'>This channel already exists.</span>
+				</div>
+				<div class="field">
+					<div id="private-channel-radio" class="radio">
+						<p class="label">Would you like this channel to be hidden or public?</p>
+						<div class="field">
+							<div class="radio">
+								<label>
+									<input type="radio" name="optionsRadios" id="optionsRadios1" value="1">
+									Keep this channel hidden please
+								</label>
+							</div>
+						</div>
+						<div class="field">
+							<div class="radio">
+								<label>
+									<input type="radio" name="optionsRadios" id="optionsRadios2" value="0" checked>
+									Make the channel public, for all to see
+								</label>
+							</div>
+						</div>
+					</div>
+				</div>
 				<?php if($_GET['error'] == "exists"){
 					echo "<div class='exists-error'><p><strong>This feed already exists in this channel</strong></p></div>";
 				}?>
-	      <input type="submit" class="btn btn-default">
+	      <input type="submit" class="button is-primary">
 	      <div class="alert alert-danger col-sm-12 hide" role="alert">
 	      <p>You must submit an RSS feed.</p>
 	      </div>
 	      </form>
 			</div>
-	    <div id="search-area" class="col-sm-6 search">
-	      <div class="form-group">
-	        <p>Please input an RSS Link or a web URL:</p>
-	        <input type="text" name="feed_link" class="form-control" id="feedLink" placeholder="RSS URL">
+	    <div id="search-area" class="column search">
+	      <div class="field">
+	        <label class="label" for="feed_link">Please input an RSS Link or a web URL:</label>
+	        <input type="text" name="feed_link" class="input" id="feedLink" placeholder="RSS URL">
 	      </div>
 	      <div class="search-results">
 	        <p>Start typing to search...</p>
